48eb997be1347956484a4c88f00ee9ffd906024b
JonanthaW <jonanthawillian@gmail.com>
2023-05-15 12:00:38 -0300

    ADDED: first commit

---
+++ CodecSoftware/Encryption.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class CODEC
+{
+    private static readonly byte[] _salt = Encoding.Unicode.GetBytes("Your random salt goes here!");
+
+    public static void Encrypt(string plainText, string password)
+    {
+        byte[] plainBytes = Encoding.Unicode.GetBytes(plainText);
+
+        using (Aes aes = Aes.Create())
+        {
+            Rfc2898DeriveBytes keyDerivation = new Rfc2898DeriveBytes(password, _salt);
+            aes.Key = keyDerivation.GetBytes(aes.KeySize / 8);
+            aes.IV = keyDerivation.GetBytes(aes.BlockSize / 8);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(plainBytes, 0, plainBytes.Length);
+                    cs.FlushFinalBlock();
+                }
+                byte[] cipherBytes = ms.ToArray();
+                Console.WriteLine(Convert.ToBase64String(cipherBytes));
+                return;
+            }
+        }
+    }
+
+    public static void Decrypt(string cipherText, string password)
+    {
+        byte[] cipherBytes = Convert.FromBase64String(cipherText);
+
+        using (Aes aes = Aes.Create())
+        {
+            Rfc2898DeriveBytes keyDerivation = new Rfc2898DeriveBytes(password, _salt);
+            aes.Key = keyDerivation.GetBytes(aes.KeySize / 8);
+            aes.IV = keyDerivation.GetBytes(aes.BlockSize / 8);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(cipherBytes, 0, cipherBytes.Length);
+                    cs.FlushFinalBlock();
+                }
+                byte[] plainBytes = ms.ToArray();
+                Console.WriteLine(Encoding.Unicode.GetString(plainBytes));
+                return;
+            }
+        }
+    }
+}
---
+++ CodecSoftware/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace CodecSoftware
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CodecType();
+        }
+
+        public static void CodecType()
+        {
+            while (true)
+            {
+                Console.WriteLine("1 - Encrypt, 2 - Decrypt");
+                Console.Write("Option: ");
+
+                if (!int.TryParse(Console.ReadLine(), out int option))
+                {
+                    Console.WriteLine("\nInvalid input. Try again.\n");
+                    continue;
+                }
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("\nEnter the phrase you want to be encrypted and a password for it:");
+                        Console.Write("Phrase: ");
+                        string phraseEncryption = Console.ReadLine();
+
+                        Console.Write("Password: ");
+                        string passwordEncryption = Console.ReadLine();
+
+                        if (string.IsNullOrWhiteSpace(phraseEncryption))
+                        {
+                            Console.WriteLine("\nInvalid input. The phrase cannot be empty or null.\n");
+                            continue;
+                        }
+
+                        CODEC.Encrypt(phraseEncryption, passwordEncryption);
+                        return;
+
+                    case 2:
+                        Console.WriteLine("\nEnter the phrase you want to be decrypted and the password of it:");
+                        Console.Write("Phrase: ");
+                        string phraseDecryption = Console.ReadLine();
+
+                        Console.Write("Password: ");
+                        string passwordDecryption = Console.ReadLine();
+
+                        if (string.IsNullOrWhiteSpace(phraseDecryption))
+                        {
+                            Console.WriteLine("\nInvalid input. The phrase cannot be empty or null.\n");
+                            continue;
+                        }
+
+                        CODEC.Decrypt(phraseDecryption, passwordDecryption);
+                        return;
+
+                    default:
+                        Console.WriteLine("\nOption is wrong/not available. Try again!\n");
+                        break;
+                }
+            }
+        }
+    }
+}
